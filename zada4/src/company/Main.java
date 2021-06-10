d35d9c42ce3e18408a7b46c99fb748f8ed590600
He4eBoLLIku <ilia777333@yandex.ru>
2021-06-10 12:34:17 +0300

    tas

---
+++ zada4/src/company/Main.java
@@ -0,0 +1,23 @@
+import com.company.Personn;
+
+public class Main{
+    public static void main(String[]args) {
+        Personn man=new Personn("SIS", 50);
+String manFullname= man.getFullname();
+int manage= man.getAge();
+        System.out.println("name"+manFullname);
+        System.out.println("vozr"+ manage);
+        System.out.println();
+        man.move();
+        man.talk();
+
+
+    }
+    }
+
+
+
+
+
+
+
